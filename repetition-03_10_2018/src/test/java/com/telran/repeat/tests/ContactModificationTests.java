6a97ec16b6d3f7c7598339a5e63dbfeaa708155a
Ferdinand101 <ferdinandverchezer@gmail.com>
2018-12-04 11:53:25 +0200

    ch

--- repetition-03_10_2018/src/test/java/com/telran/repeat/tests/ContactModificationTests.java
+++ repetition-03_10_2018/src/test/java/com/telran/repeat/tests/ContactModificationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 public class ContactModificationTests extends  TestBase {
   @Test
   public void testsContactModification(){
+    
     app.getNavigationHelper().openHomePage();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new Contact("hh", "jj", "hh", "ss", "sss"));
